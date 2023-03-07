239d612a5a1d17358695aee26152eecd06fe9249
Daniel Lu <ludaniel20110@outlook.com>
2023-03-07 20:00:43 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const image = new Image()
+image.src = './assets/ice-dragon.png'
+
+image.onload = () => {
+    draw()
+}
+
+function draw() {
+    context.drawImage(0, 0, 143, 167)
+}
