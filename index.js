503885303098340a183fed225781f5fcf102445d
Daniel Lu <ludaniel20110@outlook.com>
2023-03-07 13:23:39 +0000

    fix: V2

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function loopDraw() {
         currentOffset = 0
     }
 
-    const newTime = Math.Floor((Date.now() - startTime) / 1000)
+    const newTime = Math.floor((Date.now() - startTime) / 1000)
 
     secondsCount.innerText = newTime
 
