511a17305fa269aa5b45f325eaf34bd794f2c314
Daniel Lu <ludaniel20110@outlook.com>
2023-03-07 13:21:54 +0000

    V2

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 const canvas = document.querySelector('canvas')
+const secondsCount = document.querySelector('.seconds')
 const context = canvas.getContext('2d')
+const iceDragonDimensions = {width: 750, height: 750}
+
+const startTime = Date.now()
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+context.translate(window.innerWidth / 2, window.innerHeight / 2)
 
 const image = new Image()
-image.src = './assets/ice-dragon.png'
+image.src = './assets/ice-dragon1.png'
+
+const loopingIceDragons = 50
+const offsetDistance = 250
+let currentOffset = 0
 
 image.onload = () => {
-    draw()
+    startLooping()
 }
 
-function draw() {
-    context.drawImage(image, 0, 0, 143, 167)
+function draw(offset) {
+    context.drawImage(
+        image,
+        -iceDragonDimensions.width / 2 - offset / 2,
+        -iceDragonDimensions.height / 2 - offset / 2,
+        iceDragonDimensions.width + offset,
+        iceDragonDimensions.height + offset
+    )
+}
+
+function loopDraw() {
+    for(let i = loopingIceDragons; i >= 0; i --) {
+        draw(i * offsetDistance + currentOffset)
+    }
+
+    draw(offsetDistance)
+
+    currentOffset++
+    if (currentOffset >= offsetDistance) {
+        currentOffset = 0
+    }
+
+    const newTime = Math.Floor((Date.now() - startTime) / 1000)
+
+    secondsCount.innerText = newTime
+
+    requestAnimationFrame(loopDraw)
+}
+
+function startLooping() {
+    requestAnimationFrame(loopDraw)
 }
